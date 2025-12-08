4b70146e3a04d0438e6fba29ce137933f593a14b
VarunVF <varunraviprojects@gmail.com>
2025-12-08 21:19:57 +0800

    Implement Set using std::unordered_set instead of godot::Dictionary

--- src/set.cpp
+++ src/set.cpp
@@ -5,20 +5,25 @@ void Set::_bind_methods() {
 	godot::ClassDB::bind_method(D_METHOD("has", "element"), &Set::has);
 	godot::ClassDB::bind_method(D_METHOD("clear"), &Set::clear);
 	godot::ClassDB::bind_method(D_METHOD("erase", "element"), &Set::erase);
+    godot::ClassDB::bind_method(D_METHOD("size"), &Set::size);
 }
 
-void Set::add(const Variant &element) {
-    m_Dictionary[element] = Variant::NIL;
+void Set::add(const Variant& element) {
+    m_Set.insert(element);
 }
 
-bool Set::has(const Variant &element) const {
-    return m_Dictionary.has(element);
+bool Set::has(const Variant& element) const {
+    return m_Set.count(element) > 0;
 }
 
 void Set::clear() {
-    m_Dictionary.clear();
+    m_Set.erase(m_Set.begin(), m_Set.end());
 }
 
-void Set::erase(const Variant & element) {
-    m_Dictionary.erase(element);
+bool Set::erase(const Variant& element) {
+    return m_Set.erase(element) > 0;
+}
+
+int Set::size() const {
+	return static_cast<int>(m_Set.size());
 }
--- src/set.h
+++ src/set.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <unordered_set>
+
 #include "godot_cpp/classes/ref_counted.hpp"
 #include "godot_cpp/variant/variant.hpp"
 
@@ -8,6 +10,19 @@ using namespace godot;
 class Set : public RefCounted {
     GDCLASS(Set, RefCounted)
 
+private:
+    struct ElementHasher {
+        uint32_t operator()(const Variant& variant) const {
+            return variant.hash();
+        }
+    };
+
+    struct ElementKeyEq {
+        bool operator()(const Variant& lhs, const Variant& rhs) const {
+            return lhs == rhs;
+        }
+    };
+
 protected:
     static void _bind_methods();
 
@@ -15,11 +30,12 @@ public:
     Set() = default;
     ~Set() = default;
 
-    void add(const Variant &element);
-    bool has(const Variant &element) const;
+    void add(const Variant& element);
+    bool has(const Variant& element) const;
     void clear();
-    void erase(const Variant& element);
+    bool erase(const Variant& element);
+    int size() const;
 
 private:
-    Dictionary m_Dictionary;
+    std::unordered_set<Variant, ElementHasher, ElementKeyEq> m_Set;
 };
